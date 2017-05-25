6e2727b4543a4c90bde7e9c7e9e88ea24182a439
Cynthia Nyeint <cynthianyeint@Cynthias-MacBook-Pro.local>
2017-05-25 17:00:47 +0800

    change model

--- cxa_query/models/admission.py
+++ cxa_query/models/admission.py
@@ -3,9 +3,9 @@ from django.db import models
 class Admission (models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	description = models.CharField(max_length=200, blank=False)
-	before_admission = models.CharField(max_length=500, blank=False, null=True)
-	upon_discharge = models.CharField(max_length=500, blank=False, null=True)
-	after_discharge = models.CharField(max_length=500, blank=False, null=True)
+	before_admission = models.CharField(max_length=500, blank=True, null=True)
+	upon_discharge = models.CharField(max_length=500, blank=True, null=True)
+	after_discharge = models.CharField(max_length=500, blank=True, null=True)
 
 	class Meta:
 		ordering = ('created',)
