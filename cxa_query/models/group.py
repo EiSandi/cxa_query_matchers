f15c02a0f405927f1c543ca49658e11cdf53281d
Cynthia Nyeint <cynthianyeint@Cynthias-MacBook-Pro.local>
2017-05-30 09:37:32 +0800

    alter fields

--- cxa_query/models/group.py
+++ cxa_query/models/group.py
@@ -4,10 +4,10 @@ from cxa_query.models import *
 class Group (models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	name = models.CharField(max_length=200, blank=False)
-	category = models.CharField(max_length=100, blank=False, null=True)
-	eligibility = models.CharField(max_length=500, blank=False, null=True)
-	area_coverage = models.CharField(max_length=500, blank=False, null=True)
-	basic_coverage = models.CharField(max_length=500, blank=False, null=True)
+	category = models.CharField(max_length=100, blank=True, null=True)
+	eligibility = models.CharField(max_length=500, blank=True, null=True)
+	area_coverage = models.CharField(max_length=500, blank=True, null=True)
+	basic_coverage = models.CharField(max_length=500, blank=True, null=True)
 
 
 	class Meta:
